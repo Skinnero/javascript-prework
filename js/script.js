315c872d944623fc2eda21a56261360bcd65e2f8
Kacper Tomczyk <46459137+Skinnero@users.noreply.github.com>
2025-10-26 13:07:02 +0100

    Adding logic to player

--- js/script.js
+++ js/script.js
@@ -16,3 +16,22 @@ switch (randomNumber) {
 }
 
 printMessage("Mój ruch to: " + computerMove)
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+switch (playerInput) {
+    case '1':
+        playerMove = 'kamień'
+        break
+    case '2':
+        playerMove = 'papier'
+        break
+    case '3':
+        playerMove = 'nożyce'
+}
+
+printMessage('Twój ruch to: ' + playerMove);
